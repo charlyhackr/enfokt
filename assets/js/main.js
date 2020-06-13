19d0fe58adc3a89e45fad7306e3777a6ae3862d5
esrojas27 <62344298+esrojas27@users.noreply.github.com>
2020-06-13 17:37:08 -0500

    Actualizacion Contador

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,39 +72,35 @@
 
 })(jQuery);
 
-const getRemainTime= deadline=>{/*getRemainTime significa obtener el tiempo faltante*/
-    let now= new Date(),/*new Date() nos devuelve la fecha actual*/
-     remainTime=(new Date(deadline)-now+1000)/1000,/*remaiTime=new Date(deadline)-now tiempo que falta para 
-     llegar está fecha limite nos devuelve en milisegundo y lo dividimos en 1000 para que pasé a segundos*/
-     remainSeconds=('0'+Math.floor(remainTime % 60)).slice(-2),/*En segundos, Math.floor pasar 
-     numero entero, y slice(-2) hace seleccionar los 2 ultimos digitos*/
-     remainMinutes=('0'+Math.floor(remainTime /60 % 60)).slice(-2),
-     remainHours=('0'+Math.floor(remainTime /3600 % 24)).slice(-2),/*Cuantos segundos hay 
-    en un hora (3600) y cuantas horas hay en un día (24)*/
-    remainDays=Math.floor(remainTime / (3600 * 24));/*3600 que tiene una hora y 24 horas que
-    tiene un día*/
-     return{
-      remainTime,
-      remainSeconds,
-      remainMinutes,
-      remainHours,
-      remainDays
-     }
-};
-//console.log(getRemainTime('Oct 17 2019 15:26:43 GMT-0600'));
-const countdown=(deadline,elem,finalMessage)=>{/*finalMessage mensaje que se va a imprimir
-cuano¿to termine el tiempo*/
+    // This is an example with default parameters
 
-const el=document.getElementById(elem);/**Identificar el elemento*/
-const timerUpdate=setInterval(()=>{/*Crear la función que se va actualizar cada segundo*/
-    let t=getRemainTime(deadline);
-    el.innerHTML=`${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
-    if(t.remainTime<=1){/*Cuando se cumple va a dejar de contar hacia atras*/
-    clearInterval(timerUpdate);
-    el.innerHTML=finalMessage;
-    }
-},1000);/*1000 cada segundo se actualice*/
-};
-countdown("Jun 14 2020 23:59:59 GMT-0500","clock","La oportunidad ha terminado");
-
-
+    simplyCountdown('#clock', {
+        year: 2020, // required
+        month: 6, // required
+        day: 20, // required
+        hours: 19, // Default is 0 [0-23] integer
+        minutes: 0, // Default is 0 [0-59] integer
+        seconds: 0, // Default is 0 [0-59] integer
+        words: { //words displayed into the countdown
+            days: 'Día',
+            hours: 'Hora',
+            minutes: 'Minuto',
+            seconds: 'Segundo',
+            pluralLetter: 's'
+        },
+        plural: true, //use plurals
+        inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+        inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+        // in case of inline set to false
+        enableUtc: true, //Use UTC as default
+        onEnd: function() {
+			alert('Finalizo');
+			 return; 
+			}, //Callback on countdown end, put your own function here
+        refresh: 1000, // default refresh every 1s
+        sectionClass: 'simply-section', //section css class
+        amountClass: 'simply-amount', // amount css class
+        wordClass: 'simply-word', // word css class
+        zeroPad: false,
+        countUp: false
+});
